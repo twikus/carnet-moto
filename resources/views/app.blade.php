cbe60cf11d325d462d0ede0be580f3c87896f057
twikus <twikus@users.noreply.github.com>
2026-03-27 15:15:36 +0100

    fix(SCRUM-23): intégrer Ziggy pour la fonction route() dans Vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Carnet Moto</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @routes
     @inertiaHead
 </head>
 <body class="antialiased">
